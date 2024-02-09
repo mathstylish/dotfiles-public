2a309bf553f1e0c2f935744b51c0c2c4bc260231
Matheus <matheus.hpp135@gmail.com>
2024-02-09 17:28:31 -0300

    fix: 🐛 autopairs and tab doesn't work
    table.unpack is not working and mini.pairs it not working as expected
    with nvim-cmp

--- nvim/lua/mathstylish/plugins/coding.lua
+++ nvim/lua/mathstylish/plugins/coding.lua
@@ -50,7 +50,7 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			local has_words_before = function()
-				local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 				return col ~= 0
 					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 			end
@@ -122,10 +122,24 @@ return {
 	},
 
 	-- auto pairs
+
 	{
-		"echasnovski/mini.pairs",
-		event = "VeryLazy",
-		opts = {},
+		"windwp/nvim-autopairs",
+		event = { "InsertEnter" },
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			local autopairs = require("nvim-autopairs")
+
+			autopairs.setup({})
+
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+
+			-- make autopairs and completion work together
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
 	},
 
 	-- comments
